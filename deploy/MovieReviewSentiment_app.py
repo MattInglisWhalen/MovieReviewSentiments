0c5c3e4d856269006d57c8a9c7a9fe9359f44b9a
MattInglisWhalen <97885769+MattInglisWhalen@users.noreply.github.com>
2023-10-16 13:46:08 +0200

    Flask with CORS

--- deploy/MovieReviewSentiment_app.py
+++ deploy/MovieReviewSentiment_app.py
@@ -1,16 +1,10 @@
 import pickle
-# import numpy as np
-from flask import Flask, request
+from flask import Flask, request, make_response, jsonify
 
-import sys
-from asizeof import asizeof as better_size
+vocab = None  # HashingVectorizer
+model = None  # LogisticRegression
 
-# vocab = None  # vocab (TfidfVectorizer) is too big (~0.5GB), so let's try to work without it
-model = None
-
-vocab_dict : dict[str,int] = None
-vocab_list : list[str] = None
-
+expected_request_origin = "https://mattingliswhalen.github.io"
 app = Flask(__name__)
 
 header = """
@@ -64,101 +58,119 @@ def strength_to_color_string(strength: float, min_str: float, max_str: float) ->
         color_str += "%0.2X" % int( (max_str-strength)/max_str * 255)   # blue
     return color_str
 
-def transformed_word_to_list(transformed_word) :
-    """Turns a sparse array into a list of strings"""
-    my_list = [(vocab_list[j], transformed_word[i, j]) for i, j in zip(*transformed_word.nonzero())]
-    return my_list
+def strength_gram(*words) :
+    """Turns a list of words into a strength based on logistic regression coefficient of that 2gram"""
+    bag_of_words = vocab.fit_transform([' '.join(list(words))])
 
-def strength_1gram(bword) :
-    """Turn a binary word into a strength based on logistic regression coefficient of that word"""
-    word = bword.decode()  # bit string to string
-    word_as_sparse_arr = vocab.transform([word])
-    _, word_index_as_list = word_as_sparse_arr.nonzero()
-    if word_index_as_list.size > 0:
-        strength = model.coef_[0, word_index_as_list[0]]
-    else :
-        strength = 0
-    return strength
+    # the following will give 3 entries: 1 for the 2 1-grams and 1 for the 2-gram
+    _, indices = bag_of_words.nonzero()
+    net_strength = 0
+    for idx in indices :
+        bag_val = bag_of_words[0,idx]
+        sign = bag_val / abs(bag_val) if abs(bag_val) > 1e-10 else 0
+        net_strength += sign * model.coef_[0, idx]
 
-def strength_2gram(bword1,bword2) :
-    """Turns two binary words into a strength based on logistic regression coefficient of that 2gram"""
-    word1, word2 = bword1.decode(), bword2.decode()  # bit string to string
-    bigram_as_sparse_arr = vocab.transform([f"{word1} {word2}"])
-    _, bigram_index_as_list = bigram_as_sparse_arr.nonzero()
-    if bigram_index_as_list.size > 0 :
-        strength = model.coef_[0, bigram_index_as_list[0]]
-    else :
-        strength = 0
-    return strength
-
+    return net_strength
 
 def reasoning_html_from_string(data: str) -> str :
     """Returns html markup containing the data string with coloring based on model coefficients"""
     reasoning_str = "<p> Reasoning: "
-    min_str, max_str = min(model.coef_[0]), max(model.coef_[0])
+    # min_str, max_str = min(model.coef_[0]), max(model.coef_[0])
+    min_str, max_str = -10, 10
 
     # pad the data with meaningless buffer strings
-    data_list = [b"BBBB"]
-    data_list.extend(data.split())
-    data_list.extend([b"BBBB"])
+    data_list = ("BBBB " + data + " BBBB").split()
 
-    for n, (prev_bword, this_bword, next_bword) in enumerate( zip(data_list[:-2],data_list[1:-1],data_list[2:])):
+    for n, (prev_word, this_word, next_word) in enumerate( zip(data_list[:-2],data_list[1:-1],data_list[2:])):
 
-        this_word = this_bword.decode()
-        str_1gram = strength_1gram(this_bword)
-        str_2gram_prev = strength_2gram(prev_bword,this_bword)
-        str_2gram_next = strength_2gram(this_bword,next_bword)
+        str_prev = strength_gram(prev_word)
+        str_this = strength_gram(this_word)
+        str_next = strength_gram(next_word)
 
-        net_strength = (str_1gram+str_2gram_prev+str_2gram_next)/3
-        right_strength = (str_1gram+str_2gram_next)/3
-        reasoning_str += f"""<span style = "background-color:{strength_to_color_string(net_strength,min_str,max_str)}">{this_word}</span>"""
-        reasoning_str += f"""<span style = "background-color:{strength_to_color_string(right_strength,min_str,max_str)}"> </span>\n"""
+        str_2gram_left = strength_gram(prev_word,this_word) - str_prev - str_this
+        str_2gram_right = strength_gram(this_word,next_word) - str_this - str_next
+
+        if n == 0 :
+            str_2gram_left = (str_this + str_2gram_right)/2
+        elif n == len(data_list[:-2]) - 1 :
+            str_2gram_right = (str_2gram_left + str_this)/2
+
+        net_strength = (str_2gram_left + str_this + str_2gram_right)/3
+
+        reasoning_str += f"""<span style = "background-color:"""
+        reasoning_str += f"""{strength_to_color_string(net_strength,min_str,max_str)}">{this_word}</span>"""
+        reasoning_str += f"""<span style = "background-color:"""
+        reasoning_str += f"""{strength_to_color_string(str_2gram_right,min_str,max_str)}"> </span>\n"""
         if n % 12 == 11 :
             reasoning_str += "<br>"
     reasoning_str += "</p>"
     return reasoning_str
 
+def sanitize(data_str : bytes) -> str :
+    cleaned_str = ""
+    for char in data_str.decode() :
+        if char == '<' :
+            cleaned_str += '&lt;'
+        elif char == '>' :
+            cleaned_str += '&gt;'
+        elif char == '/':
+            cleaned_str += '&sol;'
+        elif char == '\\':
+            cleaned_str += '&bsol;'
+        else :
+            cleaned_str += char
+    return cleaned_str
+
 def load_model():
     """Load the stored inference model and vocabulary"""
     global vocab
     global model
-    global vocab_dict
-    global vocab_list
-    with open('vocabulary.pkl', 'rb') as f_v:
+
+    with open('vocabulary_hashed.pkl', 'rb') as f_v:
         vocab = pickle.load(f_v)
-    with open('sentiment_inference_model.pkl', 'rb') as f_m:
+    with open('sentiment_inference_model_hashed.pkl', 'rb') as f_m:
         model = pickle.load(f_m)
-    vocab_dict = vocab.vocabulary_
-    vocab_list = vocab.get_feature_names_out()
+
 
 @app.route('/')
 def home_endpoint():
     """Locally: what to show when visiting localhost:80"""
     return header+last_return
 
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")  # in production, only include mattingliswhalen.github.io
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
 
-@app.route('/predict', methods=['POST'])
+def _corsify_actual_response(data_str):
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.set_data(data_str)
+    print(f"Response out: {response.get_data().decode()}")
+    return response
+
+@app.route('/predict', methods=['POST','OPTIONS'])
 def get_prediction():
     """Locally: what to show when receiving a post request at localhost:80/predict"""
     """>  curl.exe -X POST localhost:80/predict -H 'Content-Type: application/json' -d 'This is a review' """
     global last_return
     # Works only for a single sample
-    if request.method == 'POST':
-        data = request.get_data()  # Get data posted as a string
-        transformed_data = vocab.transform([data])  # Transform the input string with the prefit TfIdfVectorizer
+    if request.method == 'OPTIONS' :  # CORS preflight
+        return _build_cors_preflight_response()
+    elif request.method == 'POST':
+        data = sanitize(request.get_data())  # Get data posted as a string
+        transformed_data = vocab.transform([data])  # Transform the input string with the HasheVectorizer
         sentiment = model.predict_proba(transformed_data)[0,1]  # Runs globally-loaded model on the data
         last_return = prob_to_html(sentiment) + reasoning_html_from_string(data)
+    else :
+        raise RuntimeError(f"Can't handle >{request.method}< request method")
 
-    return last_return
+    return _corsify_actual_response(last_return)
 
 
 if __name__ == '__main__':
     load_model()  # load model at the beginning once only
-
-    print(f"model: {sys.getsizeof(model)} {better_size(model) / 1e3:.1f}KB")
-    print(f"vocab: {sys.getsizeof(vocab)} {better_size(vocab) / 1e6:.1f}MB")
-    print(f"vocab dict: {sys.getsizeof(vocab_dict)} {better_size(vocab_dict)/ 1e6:.1f}MB")
-    print(f"vocab list: {sys.getsizeof(vocab_list)} {better_size(vocab_list)/ 1e6:.1f}MB")
-
     app.run(host='0.0.0.0', port=80)
 
